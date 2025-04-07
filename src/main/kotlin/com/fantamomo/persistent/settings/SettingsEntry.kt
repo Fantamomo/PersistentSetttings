2f72afaef0ddf7ee5a1def54205da62220bc9404
Fantamomo <me@fantamomo.com>
2025-04-07 13:04:09 +0200

    docs: Enhance SettingsEntry class with comprehensive documentation for properties and methods

--- src/main/kotlin/com/fantamomo/persistent/settings/SettingsEntry.kt
+++ src/main/kotlin/com/fantamomo/persistent/settings/SettingsEntry.kt
@@ -5,6 +5,14 @@ import com.fantamomo.persistent.type.PersistentDataType
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
+/**
+ * Class that holds the data of an [key].
+ *
+ * @param key The key of the entry
+ * @param type The type of the value
+ * @param node The [ParentSettingsNode] where the entry was created (It is only used for the [path] and [ParentSettingsNode.settings]
+ * @param default The lazy default initializer
+ */
 class SettingsEntry<P : Any, C : Any> internal constructor(
     internal val node: ParentSettingsNode,
     internal val type: PersistentDataType<P, C>,
@@ -12,11 +20,19 @@ class SettingsEntry<P : Any, C : Any> internal constructor(
     internal val default: () -> C,
 ) : ReadWriteProperty<ParentSettingsNode, C> {
 
+    /**
+     * The path where when saved the value should be saved and loaded
+     */
     val path: String by lazy {
         node.path + ":" + (key
             ?: throw IllegalArgumentException("No key was provided, provide a key or call getValue or setValue first"))
     }
 
+    /**
+     * Methode that return the [value] in the `by`-Syntax
+     *
+     * When the [key] is not set, it will be computed by [KProperty.name]
+     */
     override fun getValue(
         thisRef: ParentSettingsNode,
         property: KProperty<*>,
@@ -25,10 +41,19 @@ class SettingsEntry<P : Any, C : Any> internal constructor(
         return value
     }
 
+    /**
+     * Property for getting and settings the value,
+     * in [Settings] over [com.fantamomo.persistent.container.BufferedPersistentDataContainer]
+     */
     var value: C
         get() = node.settings.container.getOrDefault(path, type, default)
         set(value) = node.settings.container.set(path, type, value)
 
+    /**
+     * Methode that sets the [value] in the `by`-Syntax
+     *
+     * When the [key] is not set, it will be computed by [KProperty.name]
+     */
     override fun setValue(
         thisRef: ParentSettingsNode,
         property: KProperty<*>,
@@ -38,4 +63,11 @@ class SettingsEntry<P : Any, C : Any> internal constructor(
         this.value = value
     }
 
+    @Suppress("NOTHING_TO_INLINE")
+    inline fun set(value: C) {
+        this.value = value
+    }
+
+    @Suppress("NOTHING_TO_INLINE")
+    inline fun get() = value
 }
